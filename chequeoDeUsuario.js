6ec02ee2e1988cc097b909c9c7a561200f2f1cbe
gianella lastra <gianellalastra4@gmail.com>
2024-12-06 18:44:31 -0300

    agregue un mensaje de bienvenida si el usuario es correcto y un mensaje por si el usuario es incorrecto

--- chequeoDeUsuario.js
+++ chequeoDeUsuario.js
@@ -1,21 +1,28 @@
 const basesDeDatos = [
-    { nombre: "Juan44", contraseña: "juancarlos123" }
+    { nombre: "Juan44", contraseña: "juancarlos123" },
+    { nombre: "Sofia@gmail.com", contraseña: "sofia1234"},
+    { nombre: "MatiasSopla@gmail.com", contraseña: "riverelmejor" },
+    { nombre: "Fredoorlando@gmail.com", contraseña: "bocabocaa" },
+    { nombre: "Julianmatias12@gmail.com", contraseña: "panpan123" }
 ];
- 
+
 const formulario = document.getElementById("formulario");
 
 
 formulario.addEventListener("submit", (event) => {
-event.preventDefault()
+    event.preventDefault()
 
 
-const NombreDeUsuario = document.getElementById("nombre-de-usuario").value;
-const contraseñaDeUsuario = document.getElementById("contraseña-de-usuario").value;
+    const NombreDeUsuario = document.getElementById("nombre-de-usuario").value;
+    const contrasennaDeUsuario = document.getElementById("contraseña-de-usuario").value;
 
 
-const existeElUsuario = basesDeDatos.some( usuario => usuario.nombre === NombreDeUsuario &&  usuario.contraseña === contraseñaDeUsuario);
+    const existeElUsuario = basesDeDatos.find(
+        usuario => usuario.nombre === NombreDeUsuario && usuario.contraseña === contrasennaDeUsuario);
 
-console.log(existeElUsuario);
-});
-
-   
+    if (existeElUsuario) {
+        alert(`Bienvenido a tu cuenta ${existeElUsuario.nombre}`);
+    }else {
+        alert("No encontramos tu usuario");
+    }
+    })
