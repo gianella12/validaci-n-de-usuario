9b79c9da8c41ebbfdc841412229fff4c830ac239
gianella lastra <gianellalastra4@gmail.com>
2024-12-12 12:13:07 -0300

    agregue la logica del boton para poder ver la contraseña

--- chequeoDeUsuario.js
+++ chequeoDeUsuario.js
@@ -1,6 +1,6 @@
 const basesDeDatos = [
     { nombre: "Juan44", contraseña: "juancarlos123" },
-    { nombre: "Sofia@gmail.com", contraseña: "sofia1234"},
+    { nombre: "Sofia@gmail.com", contraseña: "sofia1234" },
     { nombre: "MatiasSopla@gmail.com", contraseña: "riverelmejor" },
     { nombre: "Fredoorlando@gmail.com", contraseña: "bocabocaa" },
     { nombre: "Julianmatias12@gmail.com", contraseña: "panpan123" }
@@ -14,7 +14,7 @@ formulario.addEventListener("submit", (event) => {
 
 
     const NombreDeUsuario = document.getElementById("nombre-de-usuario").value;
-    const contrasennaDeUsuario = document.getElementById("contraseña-de-usuario").value;
+    const contrasennaDeUsuario = document.getElementById("contrasenna-de-usuario").value;
 
 
     const existeElUsuario = basesDeDatos.find(
@@ -22,8 +22,19 @@ formulario.addEventListener("submit", (event) => {
 
     if (existeElUsuario) {
         alert(`Bienvenido a tu cuenta ${existeElUsuario.nombre}`);
-        
-    }else {
+
+    } else {
         alert("No encontramos tu usuario. Verifica el nombre de usuario o contraseña");
     }
-    })
+});
+ 
+const inputContrasenna = document.getElementById("contrasenna-de-usuario");
+const botonVerContrasenna = document.getElementById("ver-contrasenna");
+
+botonVerContrasenna.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    const esPassword = inputContrasenna.type === 'password';
+    inputContrasenna.type = esPassword ? 'text' : 'password';
+    botonVerContrasenna.textContent = esPassword ? '🤫' : '👀';
+  });
